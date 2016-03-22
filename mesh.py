eb33043866043ab9ed659bd02fae9a60d619afc3
msumera <michal.sumera@gmail.com>
2016-03-22 19:24:15 +0100

    add 2 operations on array structure

--- mesh.py
+++ mesh.py
@@ -9,44 +9,63 @@ class MeshStructure(object):
         pass
     def loadFromStl(self):
         pass
+    def vertexes(self):
+        pass
+    def vertexFaces(self):
+        pass
+    def elements(self):
+        pass
 
 
 class MeshArray(MeshStructure):
     def __init__(self):
         self.vertexes = []
+        self.connections = []
 
     def loadFromPly(self):
         pass
 
     def loadFromStl(self):
         data = DataLoader().load_file('files/example.stl')
+        self.vertexes = []
         for facet in data[1]:
             for vertex in facet['vertexes']:
                 self.vertexes.append(vertex)
         self.vertexes = list(set(self.vertexes))
         self.connections = []
-        for i in range(len(self.vertexes)):
-            self.connections.append([])
         for facet in data[1]:
             a = self.vertexes.index(facet['vertexes'][0])
             b = self.vertexes.index(facet['vertexes'][1])
             c = self.vertexes.index(facet['vertexes'][2])
-            self.connections[a].append(b)
-            self.connections[a].append(c)
-            self.connections[b].append(a)
-            self.connections[b].append(c)
-            self.connections[c].append(a)
-            self.connections[c].append(b)
-        for i in range(len(self.connections)):
-            self.connections[i] = list(set(self.connections[i]))
+            self.connections.append((a,b,c))
         print self.vertexes
         print self.connections
+
+    def vertexes(self):
+        pass
+
+    def vertexFaces(self):
+        result = dict()
+        for vertex in self.vertexes:
+            result[vertex] = []
+        for i in range(len(self.connections)):
+            for vertex in self.connections[i]:
+                result[vertex].append(i)
+
+    def elements(self):
+        result = dict()
+        for elem in self.connections:
+            result[elem] = []
+            for elem2 in self.connections:
+                if len(set(elem + elem2)) < 6:
+                    result[elem].append(elem2)
+        return result
 
 
 class MeshHalfEdge(MeshStructure): # albo winged
     def __init__(self):
         pass
-    def loadFromStl(self):
-        pass
     def loadFromPly(self):
+        pass
+    def loadFromStl(self):
         pass
