3238fd9201845f65228aa455a600d5e931022e3c
msumera <michal.sumera@gmail.com>
2016-04-02 15:36:28 +0200

    add mesh_boundary method, add DataStructure interface to face-vertex structure

--- structures/face_vertex.py
+++ structures/face_vertex.py
@@ -1,56 +1,54 @@
 from processing.loader import DataLoader
 from processing.exceptions import IncorrectFacesException
+from data_structure import DataStructure
 
 
-class FaceVertexStructure():
+class FaceVertexStructure(DataStructure):
     def __init__(self, path):
         self.raw_data = DataLoader().load_file(path)
         self.vertex_array = self.raw_data['vertex']
         self.face_array = self.raw_data['face']
 
-    def vertex_neighbors(self):
-        temp = []
-        result = []
-        for i in range(len(self.vertex_array)):
-            temp.append(set())
-            result.append(set())
-        for face in self.face_array:
-            for i in range(len(face)):
-                temp[face[i]].add(face[(i+1) % len(face)])
-                temp[face[i]].add(face[(i+len(face)-1) % len(face)])
-        for v in range(len(temp)):
-            result[v] |= temp[v]
-            for i in temp[v]:
-                result[v] |= temp[i]
+    def __vertex_neighbors(self, vertex_id):
+        faces = self.elements_with_vertex(vertex_id)
+        result = set()
+        for face in faces:
+            result.add(face[(face.index(vertex_id) + 1 + len(face)) % len(face)])
+            result.add(face[(face.index(vertex_id) - 1 + len(face)) % len(face)])
         return result
 
-    def vertex_faces(self):
-        result = []
-        for i in range(len(self.vertex_array)):
-            result.append(set())
-        for i in range(len(self.face_array)):
-            for vertex in self.face_array[i]:
-                result[vertex].add(i)
+    def vertex_surrounding(self, vertex_id):
+        temp = self.__vertex_neighbors(vertex_id)
+        result = set()
+        result += temp
+        for v in temp:
+            result += self.__vertex_neighbors(v)
         return result
 
-    def face_neighbors(self):
-        temp = []
+    def elements_with_vertex(self, vertex_id):
         result = []
-        for i in range(len(self.face_array)):
-            temp.append(set())
-            result.append(set())
-        for a in range(len(self.face_array)):
-            for b in range(a, len(self.face_array)):
-                if a != b and len(set(self.face_array[a]) & set(self.face_array[b])) > 1:
-                    temp[a].add(b)
-                    temp[b].add(a)
-        for i in range(len(self.face_array)):
-            result[i] |= temp[i]
-            for j in temp[i]:
-                result[i] |= temp[j]
+        for i in self.face_array:
+            if vertex_id in i:
+                result.append(i)
         return result
 
-    def triangle_edge_replace(self, face1, face2):
+    def __face_neighbors(self, face_id):
+        result = set()
+        face = self.face_array[face_id]
+        for f in range(len(self.face_array)):
+            if face != self.face_array[f] and len(set(face) & set(self.face_array[f])) > 1:
+                result.add(f)
+        return result
+
+    def face_surrounding(self, face_id):
+        result = set()
+        temp = self.__face_neighbors(face_id)
+        result += temp
+        for f in temp:
+            result += self.__face_neighbors(f)
+        return result
+
+    def switch_triangles(self, face1, face2):
         if face1 == face2 or \
                 len(set(self.face_array[face1]) & set(self.face_array[face2])) != 2 or \
                 len(self.face_array[face1]) != 3 or \
@@ -61,6 +59,8 @@ class FaceVertexStructure():
         self.face_array[face1] = (other_vertexes[0], other_vertexes[1], shared_vertexes[0])
         self.face_array[face2] = (other_vertexes[0], other_vertexes[1], shared_vertexes[1])
 
-    # TODO
     def mesh_boundary(self):
-        pass
+        for v in range(len(self.vertex_array)):
+            if len(self.__vertex_neighbors(v)) < 2:
+                return False
+        return True
